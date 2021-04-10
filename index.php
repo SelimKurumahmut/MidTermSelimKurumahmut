81cc38f3c9391e1395fd20b51c374fca4f4678f8
Selim Kurumahmut <1700002324@stu.iku.edu.tr>
2021-04-10 10:35:11 +0300

    adding form for entering car properties

--- index.php
+++ index.php
@@ -1,3 +1,30 @@
-<?php
-echo "Welcome to my site outsider.";
-?>
+<?php echo "Welcome to my site outsider"?>
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Document</title>
+</head>
+<body>
+    <h2>Enter Car Properties</h2>
+    <form method="POST">
+        <p> <label for="input">License plate</label>
+            <input type="text" name="licensePlate" size="40"/>
+        </p>
+        <p> <label for="input">Brand</label>
+            <input type="text" name="brand" size="40"/>
+        </p>
+        <p> <label for="input">Model</label>
+            <input type="text" name="model" size="40"/>
+        </p>
+        <p> <label for="input">Model Year</label>
+            <input type="text" name="modelYear" size="40"/>
+        </p>
+        <p> <label for="input">Color</label>
+            <input type="text" name="color" size="40"/>
+        </p>
+        <input type="submit">
+    </form>
+</body>
+</html>
